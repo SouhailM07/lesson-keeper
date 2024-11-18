57aadc344be44a6bd048973f7e71752ae7d885cd
souhail <lightshadow416@gmail.com>
2024-11-18 11:45:42 +0100

    adding new syntax to that get users

--- backend/routers/users/user.router.ts
+++ backend/routers/users/user.router.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { Request, Response } from "express";
 // ? controllers
 import {
   createUser_c,
@@ -7,10 +8,14 @@ import {
   getUser_c,
   getUsers_c,
 } from "../../controllers/user.controller";
+import User from "../../models/user.mode";
 
 const router = Router();
 
-router.get("/", getUsers_c);
+router.get("/", async (req: Request, res: Response) => {
+  let users = await User.find({});
+  res.status(200).send(users);
+});
 router.get("/:id", getUser_c);
 router.post("/create_user", createUser_c);
 router.put("/edit_user", editUser_c);
