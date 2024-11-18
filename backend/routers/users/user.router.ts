e9c4b5002a622e82a4aa8575f23a2bc9e90114e3
souhail <lightshadow416@gmail.com>
2024-11-18 11:47:31 +0100

    return to the base syntax

--- backend/routers/users/user.router.ts
+++ backend/routers/users/user.router.ts
@@ -1,5 +1,4 @@
 import { Router } from "express";
-import { Request, Response } from "express";
 // ? controllers
 import {
   createUser_c,
@@ -8,14 +7,10 @@ import {
   getUser_c,
   getUsers_c,
 } from "../../controllers/user.controller";
-import User from "../../models/user.mode";
 
 const router = Router();
 
-router.get("/", async (req: Request, res: Response) => {
-  let users = await User.find({});
-  res.status(200).send(users);
-});
+router.get("/", getUsers_c);
 router.get("/:id", getUser_c);
 router.post("/create_user", createUser_c);
 router.put("/edit_user", editUser_c);
