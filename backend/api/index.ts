f340c096fda579d6b135c0e2254bcd27fa33bafb
souhail <lightshadow416@gmail.com>
2024-11-18 13:51:34 +0100

    fixing the path and syntax for serverless-http

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -1,30 +1,4 @@
-import express, { Request, Response } from "express";
-import "dotenv/config";
-import ServerlessHttp from "serverless-http";
-import connectToDatabase from "../lib/connectDB";
-// ? imported routers
-import userRouter from "../routers/users/user.router";
+import serverless from "serverless-http";
+import app from "../server"; // Import the Express app
 
-const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-app.get("/", (req: Request, res: Response) => {
-  res.status(200).send("welcome to the absolute shadow !");
-});
-
-// ? routers
-app.use("/users", userRouter);
-
-//=============================================================================================
-// server settings
-//=============================================================================================
-
-app.listen(process.env.PORT!, () => {
-  connectToDatabase();
-  console.log("================================");
-  console.log("The server is online and ready !");
-  console.log("================================");
-});
-
-export default ServerlessHttp(app);
+export default serverless(app); // Wrap the app in serverless-http
---
+++ backend/server.ts
@@ -0,0 +1,29 @@
+import express, { Request, Response } from "express";
+import "dotenv/config";
+import connectToDatabase from "./lib/connectDB";
+// ? imported routers
+import userRouter from "./routers/users/user.router";
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).send("welcome to the absolute shadow !");
+});
+
+// ? routers
+app.use("/users", userRouter);
+
+//=============================================================================================
+// server settings
+//=============================================================================================
+
+app.listen(process.env.PORT! || 3000, () => {
+  connectToDatabase();
+  console.log("================================");
+  console.log("The server is online and ready !");
+  console.log("================================");
+});
+
+export default app;
